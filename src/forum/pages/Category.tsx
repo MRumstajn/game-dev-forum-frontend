c9846826fc82437f063ff2b00b91eb612646835d
MRumstajn <mrumstajn@student.unizd.hr>
2023-03-23 17:26:01 +0100

    Add filtering to category page

--- src/forum/pages/Category.tsx
+++ src/forum/pages/Category.tsx
@@ -1,13 +1,17 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
-import { Breadcrumbs, Button, Typography } from "@tiller-ds/core";
+import { Breadcrumbs, Button, Card, Typography } from "@tiller-ds/core";
+import { DateInput } from "@tiller-ds/date";
+import { Input } from "@tiller-ds/form-elements";
 import { Icon } from "@tiller-ds/icons";
 
+import moment from "moment/moment";
 import { Link, useParams } from "react-router-dom";
 
 import { CategoryResponse } from "../../common/api/CategoryResponse";
 import { ThreadResponse } from "../../common/api/ThreadResponse";
 import { postSearchThreadRequest } from "../../news/api/postSearchThreadRequest";
+import { SearchThreadRequest } from "../../news/api/SearchThreadRequest";
 import { getCategoryById } from "../../Thread/api/getCategoryById";
 import { postThreadStatisticsRequest } from "../api/postThreadStatisticsRequest";
 import { ThreadStatisticsResponse } from "../api/ThreadStatisticsResponse";
@@ -19,6 +23,10 @@ export function Category() {
   const [threadStatistics, setThreadStatistics] = useState<
     ThreadStatisticsResponse[]
   >([]);
+  const [filterFormOpen, setFilterFormOpen] = useState<boolean>(false);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [usernameFilter, setUsernameFilter] = useState<string>();
 
   const params = useParams();
 
@@ -30,15 +38,13 @@ export function Category() {
   }, [params.categoryId]);
 
   // get threads
-  useEffect(() => {
-    if (parentCategory === undefined) {
-      return;
-    }
-
-    postSearchThreadRequest({
-      categoryId: parentCategory.id,
-    }).then((matchedThreads) => setThreads(matchedThreads));
-  }, [parentCategory]);
+  useEffect(
+    () =>
+      updateThreadList({
+        categoryId: parentCategory?.id,
+      }),
+    [parentCategory]
+  );
 
   // get details for all threads
   useEffect(() => {
@@ -51,6 +57,16 @@ export function Category() {
     }).then((statistics) => setThreadStatistics(statistics));
   }, [threads]);
 
+  function updateThreadList(request: SearchThreadRequest) {
+    if (parentCategory === undefined) {
+      return;
+    }
+
+    postSearchThreadRequest(request).then((matchedThreads) =>
+      setThreads(matchedThreads)
+    );
+  }
+
   function getStatisticForThread(
     threadId: number
   ): ThreadStatisticsResponse | undefined {
@@ -61,6 +77,28 @@ export function Category() {
     return threadStatistics.find(
       (statistic) => statistic.threadId === threadId
     );
+  }
+
+  function filterFormSubmitHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    let request = {
+      categoryId: parentCategory?.id,
+      authorUsername: usernameFilter,
+    } as SearchThreadRequest;
+
+    if (startDate !== null) {
+      request.creationDateTimeFromIncluding = moment(startDate)
+        .add(1, "day")
+        .toDate();
+    }
+    if (endDate !== null) {
+      request.creationDateTimeToIncluding = moment(endDate)
+        .add(1, "day")
+        .toDate();
+    }
+
+    updateThreadList(request);
   }
 
   return (
@@ -83,10 +121,82 @@ export function Category() {
               <Typography variant="h1" element="h1">
                 Forum
               </Typography>
-              <Button variant="filled" color="primary">
+              <Button
+                variant="filled"
+                color="primary"
+                onClick={() => setFilterFormOpen((prevState) => !prevState)}
+              >
                 <span className="text-white">Filter</span>
               </Button>
             </div>
+            {filterFormOpen && (
+              <Card className="flex flex-col space-y-10">
+                <Card.Body className="bg-gray-200">
+                  <form onSubmit={filterFormSubmitHandler}>
+                    <div className="flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10">
+                      <div className="flex flex-row space-x-3 items-center">
+                        <Typography variant="text" element="p">
+                          By
+                        </Typography>
+                        <Input
+                          name="author"
+                          placeholder="Author"
+                          onChange={(event) =>
+                            setUsernameFilter(event.target.value)
+                          }
+                        />
+                      </div>
+                      <div className="flex flex-col space-y-5 md:space-y-0 md:flex-row md:space-x-10">
+                        <div className="flex flex-row space-x-3 items-center">
+                          <Typography variant="text" element="p">
+                            Start date
+                          </Typography>
+                          <DateInput
+                            name="startDate"
+                            onChange={setStartDate}
+                            value={startDate}
+                            maxDate={endDate === null ? new Date() : endDate}
+                            onReset={() => setStartDate(null)}
+                          />
+                        </div>
+                        <div className="flex flex-row space-x-3 items-center">
+                          <Typography variant="text" element="p">
+                            End date
+                          </Typography>
+                          <DateInput
+                            name="endDate"
+                            onChange={setEndDate}
+                            value={endDate}
+                            minDate={
+                              startDate === null ? new Date() : startDate
+                            }
+                            onReset={() => setEndDate(null)}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col space-y-3 sm:flex-row sm:space-y-0 sm:space-x-3 justify-end mt-10">
+                      <Button
+                        variant="filled"
+                        color="danger"
+                        className="w-full sm:w-fit"
+                        onClick={() => setFilterFormOpen(false)}
+                      >
+                        Cancel
+                      </Button>
+                      <Button
+                        variant="filled"
+                        color="primary"
+                        className="w-full sm:w-fit"
+                        type="submit"
+                      >
+                        Apply
+                      </Button>
+                    </div>
+                  </form>
+                </Card.Body>
+              </Card>
+            )}
             <div className="flex flex-col space-y-3">
               <div className="grid grid-cols-3">
                 <Typography variant="text" element="p">
